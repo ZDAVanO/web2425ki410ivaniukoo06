06cb22eda31cc1934125eb6018ccec568efe7f4c
Oleh <olehivaniuk.net@gmail.com>
2025-03-06 18:30:16 +0200

    update public path

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.set('view engine', 'ejs'); // Додаємо цей рядок для нал
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // app.get('/', (req, res) => {
